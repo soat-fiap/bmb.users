ac432eaeb9262de7ae277fca05edd02978d2fc20
italo <italo.pessoa@hotmail.com>
2024-08-25 11:28:13 -0300

    change identity pool name

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_cognito_user_pool_client" "bmb_test_client" {
 }
 
 resource "aws_cognito_identity_pool" "bmb_identity_pool" {
-  identity_pool_name               = "identity pool"
+  identity_pool_name               = "bmb_identity_pool"
   allow_unauthenticated_identities = false
   cognito_identity_providers {
     client_id               = aws_cognito_user_pool_client.bmb_test_client.id
@@ -54,6 +54,7 @@ resource "aws_cognito_identity_pool" "bmb_identity_pool" {
 
   tags = {
     Terraform = "true"
+    TCManaged = "true"
   }
 }
 
@@ -65,6 +66,7 @@ resource "aws_cognito_identity_pool_provider_principal_tag" "identity_provider_t
     test = "value"
   }
 }
+
 
 resource "aws_cognito_user_pool_domain" "main" {
   domain       = "bmb-users-fiap"
